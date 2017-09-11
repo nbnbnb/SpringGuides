dd72c6482c57edb502d3e34eca10b91eca2f77d8
ZhangJin <nbnbnb.zhang@gmail.com>
2017-09-11 23:19:58 +0800

    添加虚拟路径

--- SchedulingTasks/src/main/java/hello/AppSchedulingTasks.java
+++ SchedulingTasks/src/main/java/hello/AppSchedulingTasks.java
@@ -9,16 +9,16 @@ import java.time.LocalDateTime;
 
 @SpringBootApplication
 @EnableScheduling
-public class Application {
+public class AppSchedulingTasks {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(AppSchedulingTasks.class);
+    }
 
     @PostConstruct
     void init() {
         // 在 SchedulingTasks 目录下运行 mvn:srping-boot:run
         // 当修改类文件时，会自动编译加载
-        System.out.println("System is Running... " + LocalDateTime.now());
-    }
-
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class);
+        System.out.println("-----hello world------" + LocalDateTime.now());
     }
 }
--- Thymeleaf/src/main/java/hello/GreetingController.java
+++ Thymeleaf/src/main/java/hello/GreetingController.java
@@ -5,12 +5,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDateTime;
+
 @Controller
 public class GreetingController {
 
     @RequestMapping("/greeting")
     public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
+        model.addAttribute("today", LocalDateTime.now());
         return "greeting";
     }
 
